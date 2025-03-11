b9fe895fd2fa82fb7a3dea183af95f4a0abb7b43
renatossanches <renato.sanches2004@gmail.com>
2025-03-11 12:08:16 -0300

    Feature - Preparando ambiente POM.xml

---
+++ Estoque/src/main/java/com/Estoque/Application.java
@@ -0,0 +1,16 @@
+package com.Estoque;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@EntityScan("com.Estoque")
+@ComponentScan("com.Estoque")
+@SpringBootApplication(scanBasePackages = "com.Estoque")
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
