b10aec882b0cf7312f1d6341885c3a6dbbf3b833
renatossanches <renato.sanches2004@gmail.com>
2025-03-11 16:14:52 -0300

    Feature - Novas Configurações para unificar Spring Boot + Java FX

---
+++ Estoque/src/main/java/com/Estoque/EstoqueApp.java
@@ -0,0 +1,52 @@
+// 
+// Decompiled by Procyon v0.6.0
+// 
+
+package com.Estoque;
+
+import java.util.Map;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.Estoque.api.LoadFonts;
+import com.Estoque.api.LoadScreen;
+import com.Estoque.controllerFXML.ItemControllerFXML;
+import com.Estoque.controllerFXML.LoginController;
+import com.Estoque.repositories.TokenAuthentication;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class EstoqueApp extends Application
+{
+    private ConfigurableApplicationContext context;
+    private Map<String, String> tokens;
+    private String refreshToken;
+    
+    
+    public void init() {
+        String[] args = this.getParameters().getRaw().toArray(new String[0]);
+        LoadScreen.setContext(this.context = new SpringApplicationBuilder(new Class[0]).sources(new Class[] { com.Estoque.Application.class }).run(args));
+    }
+    
+    public void start(Stage stage) throws RuntimeException {
+        LoadFonts.todasAsFontes();
+        if (TokenAuthentication.isUserLoggedIn()) {
+            LoadScreen.showScreen("Controle de Estoque", ItemControllerFXML.class, stage, "ico");
+        }
+        else {
+            LoadScreen.showScreen("Login", LoginController.class, stage, "login");
+        }
+    }
+    
+    public void stop() {
+        this.context.close();
+        Platform.exit();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Estoque/src/main/java/com/Estoque/Main.java
@@ -0,0 +1,7 @@
+package com.Estoque;
+
+public class Main {
+	public static void main(String[] args) {
+		EstoqueApp.main(args);
+	}
+}
