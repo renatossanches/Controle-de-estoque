4458f7a34afc67e9858c6a46a8e6ea7a13c5718e
renatossanches <renato.sanches2004@gmail.com>
2025-03-11 17:08:33 -0300

    Fix - correção de bug para inicialização da classe main

--- Estoque/src/main/java/com/Estoque/Application.java
+++ Estoque/src/main/java/com/Estoque/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
 	public static void main(String[] args) {
-		
+		javafx.application.Application.launch(EstoqueApp.class, args);
 
 	}
 }
